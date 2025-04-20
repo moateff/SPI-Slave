126f96b374a36ae98cb4c48011d32d6b290a8709
moateff <moatef.yousef@gmail.com>
2025-04-20 17:46:48 +0200

    Update

--- sim/SPI_Slave_tb.sv
+++ sim/SPI_Slave_tb.sv
@@ -5,6 +5,8 @@ module SPI_Slave_tb;
     int pass_count  = 0;
     int error_count = 0;
     
+    parameter VERBOSITY = "MEDIUM"; // "MEDIUM" or "DEBUG" or "NONE"
+    
     parameter CLK_PERIOD = 10;
     
     parameter MEM_DEPTH   = 256;
@@ -51,13 +53,13 @@ module SPI_Slave_tb;
     task parallel_to_serial;
         input [TX_FRAME_WIDTH - 1:0] bus_data;
         begin
-            $display("Starting Parallel-to-Serial Conversion. Input Data: %b", bus_data);
+            msg($sformatf("Starting Parallel-to-Serial Conversion. Input Data: %b", bus_data), "DEBUG");
             for (int i = 0; i < TX_FRAME_WIDTH; i++) begin
                 MOSI = bus_data[TX_FRAME_WIDTH - 1 - i]; // Sending MSB first
-                $display("Bit %0d/%0d -> MOSI: %b", i + 1, TX_FRAME_WIDTH, MOSI);
+                msg($sformatf("Bit %0d/%0d -> MOSI: %b", i + 1, TX_FRAME_WIDTH, MOSI), "DEBUG");
                 wait_cycles(1);
             end
-            $display("Parallel-to-Serial Conversion Completed.");
+            msg("Parallel-to-Serial Conversion Completed.", "DEBUG");
         end
     endtask
 
@@ -66,13 +68,13 @@ module SPI_Slave_tb;
         output [RX_FRAME_WIDTH - 1:0] bus_data;
         begin
             bus_data = 0;
-            $display("Starting Serial-to-Parallel Conversion...");
+            msg("Starting Serial-to-Parallel Conversion...", "DEBUG");
             for (int i = 0; i < RX_FRAME_WIDTH; i++) begin
                 bus_data[RX_FRAME_WIDTH - 1 - i] = MISO; // Storing MSB first
-                $display("Bit %0d/%0d Received -> MISO: %b", i + 1, RX_FRAME_WIDTH, MISO);
+                msg($sformatf("Bit %0d/%0d Received -> MISO: %b", i + 1, RX_FRAME_WIDTH, MISO), "DEBUG");
                 wait_cycles(1);
             end
-            $display("Serial-to-Parallel Conversion Completed. Output Data: %b", bus_data);
+            msg($sformatf("Serial-to-Parallel Conversion Completed. Output Data: %b", bus_data), "DEBUG");
         end
     endtask
     
@@ -103,12 +105,13 @@ module SPI_Slave_tb;
         input [CTRL_WIDTH  - 1:0] ctrl_bits;
         input [FRAME_WIDTH - 1:0] tx_data;
         begin
-            $display("Master TX Started. Control Bits: %b, Data: %b", ctrl_bits, tx_data);
+            msg($sformatf("Master TX Started. Control Bits: %b, Transmitted Data: %b", ctrl_bits, tx_data), "MEDIUM");
             assert_slave_select;
-            $display("Slave Select Asserted.");
+            msg("Slave Select Asserted.", "DEBUG");
             parallel_to_serial({ctrl_bits, tx_data});
             deassert_slave_select;
-            $display("Slave Select Deasserted. Master TX Completed.");
+            msg("Slave Select Deasserted.", "DEBUG");
+            msg("Master TX Completed.\n", "MEDIUM");
         end
     endtask
 
@@ -117,17 +120,17 @@ module SPI_Slave_tb;
         input [FRAME_WIDTH - 1:0] tx_data;
         output [FRAME_WIDTH - 1:0] rx_data;
         begin
-            $display("Master RX Started.");
+            msg($sformatf("Master RX Started. Control Bits: %b", ctrl_bits), "MEDIUM");
             assert_slave_select;
-            $display("Slave Select Asserted.");
-            $display("Initiating Read Data Request. Control Bits: %b, Data: %b", ctrl_bits, tx_data);
+            msg("Slave Select Asserted.", "DEBUG");
+            msg($sformatf("Initiating Read Data Request. Control Bits: %b, Data: %b", ctrl_bits, tx_data), "DEBUG");
             parallel_to_serial({ctrl_bits, tx_data});
             wait_cycles(2);
-            $display("Receiving Data...");
+            msg("Receiving Data...", "DEBUG");
             serial_to_parallel(rx_data);
             deassert_slave_select;
-            $display("Slave Select Deasserted. Master RX Completed.");
-            $display("Received Data: %d", rx_data);
+            msg("Slave Select Deasserted.", "DEBUG");
+            msg($sformatf("Master RX Completed. Received Data: %d\n", rx_data), "MEDIUM");
         end
     endtask
     
@@ -139,31 +142,30 @@ module SPI_Slave_tb;
         begin    
             case (operation)
                 "write_addr": begin
-                    $display("Master Operation: Transmitting Write Address: %d", tx_data);
+                    msg($sformatf("Master Operation: Transmitting Write Address: %d", tx_data), "MEDIUM");
                     ctrl_bits = 3'b000;
                     master_tx(ctrl_bits, tx_data);
                 end
                 "write_data": begin
-                    $display("Master Operation: Transmitting Write Data: %d", tx_data);
+                    msg($sformatf("Master Operation: Transmitting Write Data: %d", tx_data), "MEDIUM");
                     ctrl_bits = 3'b001;
                     master_tx(ctrl_bits, tx_data);
                 end
                 "read_addr": begin
-                    $display("Master Operation: Transmitting Read Address: %d", tx_data);
+                    msg($sformatf("Master Operation: Transmitting Read Address: %d", tx_data), "MEDIUM");
                     ctrl_bits = 3'b110;
                     master_tx(ctrl_bits, tx_data);
                 end
                 "read_data": begin
-                    $display("Master Operation: Receiving Read DATA");
+                    msg("Master Operation: Receiving Read data", "MEDIUM");
                     ctrl_bits = 3'b111;
                     master_rx(ctrl_bits, tx_data, rx_data); // tx_data: Garbage value sent
                 end
                 default: begin
-                    $display("ERROR: Invalid operation '%s'", operation);
+                    msg($sformatf("[ERROR] Invalid operation '%s'", operation), "NONE");
                     return;
                 end
             endcase
-            $display("");
         end
     endtask
     
@@ -177,12 +179,12 @@ module SPI_Slave_tb;
             expected_data = DUT.ram_inst.mem[addr];
     
             if (expected_data === recieved_data) begin
-                $display("Data Match: Addr = %d, Expected = %d, Received = %d", 
-                          addr, expected_data, recieved_data);
+                msg($sformatf("[MATCH] Addr = %d, Expected = %d, Received = %d", 
+                          addr, expected_data, recieved_data), "NONE");
                 pass_count++;
             end else begin
-                $display("DATA MISMATCH! Addr = %d, Expected = %d, Received = %d", 
-                          addr, expected_data, recieved_data);
+                msg($sformatf("[MISMATCH] Addr = %d, Expected = %d, Received = %d", 
+                          addr, expected_data, recieved_data), "NONE");
                 error_count++;
             end
         end
@@ -191,15 +193,15 @@ module SPI_Slave_tb;
     // Test execution
     initial begin
         initialize_spi_slave;
-        $display("\nSPI Slave Initialized");
+        msg("SPI Slave Initialized", "MEDIUM");
         
         assert_reset;
-        $display("Reset Asserted");
+        msg("Reset Asserted", "MEDIUM");
         
         wait_cycles(1);
     
         for (int i = 1; i <= TEST_CASES; i++) begin
-            $display("\n-------------------------- Test Case %0d --------------------------\n", i);
+            msg($sformatf("\n-------------------------- Test Case %0d --------------------------\n", i), "NONE");
     
             // Generate random values for transactions
             master_tx_addr = $random;
@@ -217,9 +219,9 @@ module SPI_Slave_tb;
         end
        
         wait_cycles(1);
-        $display("\n-----------------------------------------------------------------------\n");
-        $display("Simulation Completed: %0d test cases excuted.", TEST_CASES);
-        $display("Test Summary: Passed: %0d, Failed: %0d", pass_count, error_count);
+        msg("\n-----------------------------------------------------------------\n", "NONE");
+        msg($sformatf("Simulation Completed: %0d test cases excuted.", TEST_CASES), "NONE");
+        msg($sformatf("Test Summary: Passed: %0d, Failed: %0d", pass_count, error_count), "NONE");
            
         $stop;
     end
@@ -237,5 +239,25 @@ module SPI_Slave_tb;
         end
     endtask
 
-        
+    function void msg(input string msg, input string msg_verbosity);
+        if (VERBOSITY == "DEBUG") begin
+            if (msg_verbosity == "DEBUG" | msg_verbosity == "MEDIUM" | msg_verbosity == "NONE") begin
+                $display("%s", msg);
+                // $display("        Time: %0t", $time);
+            end
+        end else if (VERBOSITY == "MEDIUM") begin
+            if (msg_verbosity == "MEDIUM" | msg_verbosity == "NONE") begin
+                $display("%s", msg);
+                // $display("         Time: %0t", $time);
+            end
+        end else if (VERBOSITY == "NONE") begin
+            if (msg_verbosity == "NONE") begin
+                $display("%s", msg);
+                // $display("       Time: %0t", $time);
+            end
+        end else begin
+            $display("[UNKNOWN VERBOSITY \"%s\"]", VERBOSITY);
+        end
+    endfunction
+
 endmodule
